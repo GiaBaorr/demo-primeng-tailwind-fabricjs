4b0d98cc9f646b94d4f38ac4af3e79455b1920b7
Thống ĐẶNG HOÀNG <thongdh3401@gmail.com>
2024-10-26 15:51:50 +0700

    feat: install primeng & primeicons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {RippleModule} from "primeng/ripple";
+import {ButtonModule} from "primeng/button";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
+const primeModules = [
+  ButtonModule,
+  RippleModule
+];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    ...primeModules,
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
